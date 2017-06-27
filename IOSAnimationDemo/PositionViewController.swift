1e2231d6415ab995182292d8e71d4c35a5d9a162
meZS <798506095@qq.com>
2017-06-27 20:56:21 +0800

    Add 远程仓库.

--- IOSAnimationDemo/PositionViewController.swift
+++ IOSAnimationDemo/PositionViewController.swift
@@ -10,12 +10,43 @@ import UIKit
 
 class PositionViewController: UIViewController {
 
+    @IBOutlet weak var RedView: UIView!
+    @IBOutlet weak var BlueView: UIView!
+    @IBOutlet weak var PositionView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+          super.viewDidAppear(true)
+        
+        
+        UIView.animate(withDuration: 1) { 
+            
+            self.PositionView.center.x = self.view.bounds.width - self.PositionView.center.x
+         
+        
+            UIView.animate(withDuration: 1, delay: 0.5, options: UIViewAnimationOptions.curveEaseIn, animations: {
+                
+                
+                self.BlueView.center.y = self.view.bounds.height - self.BlueView.center.y
+                
+                
+            }, completion: nil)
+            
+            
+            UIView.animate(withDuration: 1, delay: 1, options: UIViewAnimationOptions.curveEaseIn, animations: {
+                
+                self.RedView.center.y = self.view.bounds.height - self.RedView.center.y
+                self.RedView.center.x = self.view.bounds.width - self.RedView.center.x
+            
+                
+            }, completion: nil)
+            
+    }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
